456740592372a2917f947405035058857575b81e
alexandergf <alexgferreiro@hotmail.com>
2018-10-09 09:07:04 +0200

    Afegim la classe de view holder

---
+++ app/src/main/java/com/example/alexandergf/countrylist/CountryViewHolder.java
@@ -0,0 +1,18 @@
+package com.example.alexandergf.countrylist;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+public class CountryViewHolder extends RecyclerView.ViewHolder{
+    private TextView country_view;
+
+    public CountryViewHolder(View itemView) {
+        super(itemView);
+        this.country_view = itemView.findViewById(R.id.country_view);
+    }
+
+    public void bind(String country){
+        this.country_view.setText(country);
+    }
+}
