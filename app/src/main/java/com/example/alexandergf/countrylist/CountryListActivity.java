e361bcd7fe63bdeeb023e2a86cdc3d410008b6c8
alexandergf <alexgferreiro@hotmail.com>
2018-10-11 13:39:15 +0200

    Returnem el resultat de la llista de paisos i el recollim correctament.

--- app/src/main/java/com/example/alexandergf/countrylist/CountryListActivity.java
+++ app/src/main/java/com/example/alexandergf/countrylist/CountryListActivity.java
@@ -1,10 +1,12 @@
 package com.example.alexandergf.countrylist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -30,5 +32,17 @@ public class CountryListActivity extends AppCompatActivity {
         country_list_view.setAdapter(adapter);
 
         country_list_view.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
+
+        adapter.setOnClickListener(new CountryListAdapter.OnClickListener() {
+            @Override
+            public void onClick(int position) {
+                //Donem el resultat a la activitat que ens hagi cridat.
+                String country =countries.get(position);
+                Intent data = new Intent();
+                data.putExtra("country",country);
+                setResult(RESULT_OK,data);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/alexandergf/countrylist/JustCountryActivity.java
+++ app/src/main/java/com/example/alexandergf/countrylist/JustCountryActivity.java
@@ -5,10 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class JustCountryActivity extends AppCompatActivity {
+
+    public static final int CHOOSE_COUNTRY = 0;
+
+    private String country=""; //model
 
     private Button btn_choose;
     private TextView country_view;
@@ -25,9 +30,28 @@ public class JustCountryActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent= new Intent(JustCountryActivity.this, CountryListActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent, CHOOSE_COUNTRY);
             }
         });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode){
+            case CHOOSE_COUNTRY:
+                if (resultCode==RESULT_OK){
+                   country=data.getStringExtra("country");
+                   if (country!=null) {
+                       country_view.setText(country);
+                   }
+                }
+                break;
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+                break;
+        }
+
 
     }
 }
